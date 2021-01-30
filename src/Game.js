a946aa0c64680f2bf99c5e7deea79c5fed144be8
shaunaksinha23 <ssinha23@bu.edu>
2021-01-30 16:50:22 -0500

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -31,15 +31,7 @@ class Square extends React.Component {
             {this.renderSquare(5)}
             {this.renderSquare(6)}
           </div>
-          <div className="board-row">
-          {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            {this.renderSquare(6)}
-          </div>
+          
           <div className="board-row">
           {this.renderSquare(7)}
             {this.renderSquare(8)}
@@ -76,7 +68,15 @@ class Square extends React.Component {
             {this.renderSquare(33)}
             {this.renderSquare(34)}
           </div>
-          
+          <div className="board-row">
+          {this.renderSquare(35)}
+            {this.renderSquare(36)}
+            {this.renderSquare(37)}
+            {this.renderSquare(38)}
+            {this.renderSquare(39)}
+            {this.renderSquare(40)}
+            {this.renderSquare(41)}
+          </div>
         </div>
       );
     }
